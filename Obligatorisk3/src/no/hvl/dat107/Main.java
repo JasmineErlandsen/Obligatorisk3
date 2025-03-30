357a6f2ed74b22d1dd23124c269b7f36a1910eb4
h573042 <jasmine.erlandsen@hotmail.com>
2025-03-30 15:52:39 +0200

    Update
    Lagt inn kommentar, koblet opp mot databasen

--- Obligatorisk3/src/no/hvl/dat107/Main.java
+++ Obligatorisk3/src/no/hvl/dat107/Main.java
@@ -15,23 +15,23 @@ public class Main {
         Ansatt a2 = ansattDAO.finnAnsattMedId(2);
         a2.skrivUtMedProsjekter();
 
-        Prosjekt p2 = prosjektDAO.finnProsjektMedId(2);
-        p2.skrivUtMedAnsatte();
-
-        Prosjekt p3 = prosjektDAO.finnProsjektMedId(3);
-        p3.skrivUtMedAnsatte();
-
-        ansattDAO.registrerProsjektdeltagelse(a2.getAnsattID(), p3.getId());
-        a2 = ansattDAO.finnAnsattMedId(2);
-        p3 = prosjektDAO.finnProsjektMedId(3);
-        a2.skrivUtMedProsjekter();
-        p3.skrivUtMedAnsatte();
-
-        ansattDAO.slettProsjektdeltagelse(a2.getAnsattID(), p3.getId());
-        a2 = ansattDAO.finnAnsattMedId(2);
-        p3 = prosjektDAO.finnProsjektMedId(3);
-        a2.skrivUtMedProsjekter();
-        p3.skrivUtMedAnsatte();
-    }
+//        Prosjekt p2 = prosjektDAO.finnProsjektMedId(2);
+//        p2.skrivUtMedAnsatte();
+//
+//        Prosjekt p3 = prosjektDAO.finnProsjektMedId(3);
+//        p3.skrivUtMedAnsatte();
+//
+//        ansattDAO.registrerProsjektdeltagelse(a2.getAnsattID(), p3.getId());
+//        a2 = ansattDAO.finnAnsattMedId(2);
+//        p3 = prosjektDAO.finnProsjektMedId(3);
+//        a2.skrivUtMedProsjekter();
+//        p3.skrivUtMedAnsatte();
+//
+//        ansattDAO.slettProsjektdeltagelse(a2.getAnsattID(), p3.getId());
+//        a2 = ansattDAO.finnAnsattMedId(2);
+//        p3 = prosjektDAO.finnProsjektMedId(3);
+//        a2.skrivUtMedProsjekter();
+//        p3.skrivUtMedAnsatte();
+      }
 
 }
