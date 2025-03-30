0741b37ec3e63850ea5dbde8db8d0437f636d05d
orits-mads <mads.oritsland@gmail.com>
2025-03-30 20:32:54 +0200

    Tester getmetoder i main for Ansatt

--- Obligatorisk3/src/no/hvl/dat107/Main.java
+++ Obligatorisk3/src/no/hvl/dat107/Main.java
@@ -13,8 +13,22 @@ public class Main {
         ProsjektDAO prosjektDAO = new ProsjektDAO();
 
         Ansatt a2 = ansattDAO.finnAnsattMedId(2);
-        a2.skrivUt("");
 
+        // Test enkel ansatt-informasjon
+        a2.skrivUt("");
+        System.out.println("\n"); // Legger til mellomrom for bedre lesbarhet
+
+        // Test get-metoder og skriv ut detaljert informasjon
+        System.out.println("--- Detaljert Ansattinformasjon ---");
+        System.out.println("Ansatt ID: " + a2.getAnsattID());
+        System.out.println("Brukernavn: " + a2.getBrukernavn());
+        System.out.println("Fornavn: " + a2.getFornavn());
+        System.out.println("Etternavn: " + a2.getEtternavn());
+        System.out.println("Stilling: " + a2.getStilling());
+        System.out.println("Avdeling: " + a2.getAvdeling());
+        System.out.println("Månedslønn: " + a2.getManedslonn());
+        System.out.println("Ansettelsesdato: " + a2.getAnsettelsesDato());
+        System.out.println("Er sjef: " + (a2.isErSjef() ? "Ja" : "Nei"));
 
 
 //        Prosjekt p2 = prosjektDAO.finnProsjektMedId(2);
